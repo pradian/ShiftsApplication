b948bc5b812727135746080fe58257eab1b54750
Alexandru Petre <123980670+pradian@users.noreply.github.com>
2024-01-05 17:30:27 +0200

    fetching user data

--- src/app/pages/user-profile/user-profile.component.ts
+++ src/app/pages/user-profile/user-profile.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Auth } from '@angular/fire/auth';
+import {
+  Firestore,
+  Timestamp,
+  collection,
+  getDocs,
+} from '@angular/fire/firestore';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { FirebaseAuthService } from 'src/app/utilitis/services/firebase-auth.service';
 
@@ -11,43 +17,66 @@ import { FirebaseAuthService } from 'src/app/utilitis/services/firebase-auth.ser
 export class UserProfileComponent implements OnInit {
   userProfileForm: FormGroup;
   authState = 'Loading....';
-  userData: any;
+  userData?: Member;
   userId = localStorage.getItem('userId');
+  usersData: Member[] = [];
 
   constructor(
     private fb: FormBuilder,
     private authService: FirebaseAuthService,
-    private auth: Auth
+    private auth: Auth,
+    protected firestore: Firestore
   ) {
     this.userProfileForm = this.fb.group({
-      firstName: ['', Validators.required],
-      lastName: ['', Validators.required],
-      role: ['', Validators.required],
-      // Add other fields as needed
+      firstName: ['', Validators.min(3)],
+      lastName: ['', Validators.min(3)],
+      role: [''],
+      birthDate: ['', Validators.required],
+      email: ['', Validators.email],
     });
   }
 
   async ngOnInit(): Promise<void> {
-    await this.readData();
+    // await this.readData();
+    this.autofillForm();
   }
   async readData() {
+    const usersDBCol = collection(this.firestore, 'users');
+    const fetchedUsers: Member[] = [];
+    const querySnapshot = await getDocs(usersDBCol);
+    querySnapshot.forEach((doc) => {
+      fetchedUsers.push(doc.data() as Member);
+    });
+
+    return fetchedUsers;
+  }
+
+  async autofillForm() {
     try {
-      const users = await this.authService.getData('users'),
-        user = users.find((user: { uid: string }) => {
-          user.uid === this.userId;
-        });
-      if (user) {
-        this.userData = user;
-        this.userProfileForm.patchValue({
-          firstName: this.userData.firstName || '',
-          lastName: this.userData.lastName || '',
-          role: this.userData.role || '',
-        });
+      const fetchedUsers = await this.readData();
+
+      if (fetchedUsers.length > 0) {
+        const currentUser = fetchedUsers.find(
+          (user) => user.uid === this.userId
+        );
+        if (currentUser) {
+          this.userData = currentUser;
+
+          this.userProfileForm.patchValue({
+            firstName: this.userData.firstName || '',
+            lastName: this.userData.lastName || '',
+            role: this.userData.role || '',
+            birthDate:
+              this.formatISODate(this.userData.birthDate.toDate()) || '',
+            email: this.userData.email || '',
+          });
+        }
       }
     } catch (error) {
-      console.error('Error Fetching user data:', error);
+      console.error('Error fetching user data:', error);
     }
   }
+
   saveProfile(): void {
     if (this.userProfileForm.valid) {
       const { firstName, lastName, role } = this.userProfileForm.value;
@@ -56,18 +85,28 @@ export class UserProfileComponent implements OnInit {
         .then((result) => {
           if (result) {
             console.log('Profile updated successfully!');
-            // Optionally, perform additional actions upon successful profile update
           } else {
             console.error('Failed to update profile.');
-            // Handle errors or show error messages
           }
         })
         .catch((error) => {
           console.error('Error updating profile:', error);
-          // Handle errors or show error messages
         });
     } else {
-      // Handle form validation errors
     }
   }
+  formatISODate(date: Date): string {
+    const year = date.getFullYear();
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const day = date.getDate().toString().padStart(2, '0');
+    return `${year}-${month}-${day}`;
+  }
 }
+type Member = {
+  birthDate: Timestamp;
+  firstName: string;
+  lastName: string;
+  email: string;
+  role: string;
+  uid: string;
+};
