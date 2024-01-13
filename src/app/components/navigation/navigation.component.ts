c426094bafb9a37907a813f8849521bd3254d0a1
Alexandru Petre <123980670+pradian@users.noreply.github.com>
2024-01-13 20:30:47 +0200

    1

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -75,7 +75,12 @@ export class NavigationComponent implements OnInit, OnDestroy {
         this.updateLoginStatus();
 
         this.router.navigate(['/login']);
-        this._snackBar.open('Successfuly logged out');
+        this._snackBar.open('Successfuly logged out.', 'Close', {
+          duration: 3000,
+          horizontalPosition: 'end',
+          verticalPosition: 'top',
+          panelClass: ['custom-snackBar', 'snackbar-success'],
+        });
       })
       .catch((error) => {
         console.log('Error');
